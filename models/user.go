0764817f925350ea07b96cf3417db2ba8744bfe9
Jhamm <julianhamm1@gmail.com>
2019-03-17 22:49:59 +0000

    Changing the models

--- models/user.go
+++ models/user.go
@@ -1,13 +1,13 @@
 package models
 
-import "gopkg.in/mgo.v2/bson"
-
 type (
 	// User represents the structure of our resource
 	User struct {
-		Id     bson.ObjectId `json:"id" bson:"_id"`
-		Name   string        `json:"name" bson:"name"`
-		Gender string        `json:"gender" bson:"gender"`
-		Age    int           `json:"age" bson:"age"`
+		//Id        bson.ObjectId `json:"id" bson:"_id"`
+		FirstName string `json:"firstName" bson:"firstName"`
+		LastName  string `json:"lastName" bson:"lastName"`
+		Message   string `json:"message" bson:"message"`
+		Email     string `json:"email" bson:"email"`
+		Service   string `json:"service" bson:"service"`
 	}
 )
