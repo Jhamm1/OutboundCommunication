2b0ce576c3a6da466871698f3cb04c2c0eea87d4
Jhamm <julianhamm1@gmail.com>
2019-03-19 00:47:25 +0000

    Test 1

--- models/communication.go
+++ models/communication.go
@@ -2,15 +2,8 @@ package models
 
 import "gopkg.in/mgo.v2/bson"
 
-// Represents a movie, we uses bson keyword to tell the mgo driver how to name
+// Represents a communication, we uses bson keyword to tell the mgo driver how to name
 // the properties in mongodb document
-// type Movie struct {
-// 	ID          bson.ObjectId `bson:"_id" json:"id"`
-// 	Name        string        `bson:"name" json:"name"`
-// 	CoverImage  string        `bson:"cover_image" json:"cover_image"`
-// 	Description string        `bson:"description" json:"description"`
-// }
-
 type Communication struct {
 	ID        bson.ObjectId `bson:"_id" json:"id"`
 	FirstName string        `bson:"firstName" json:"firstName"`
